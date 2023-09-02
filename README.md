b4175cc6f89d0b3152d00583386679584d9d2351
Asa Paparo <asapaparo@gmail.com>
2023-09-02 02:01:14 -0400

    prettier

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # [The NYC Alliance](https://nyc-alliance.vercel.app/)
+
 A student-run organiaztion dedicated to uplifting students via FIRST robotics.
 
 This is our website! It's made with [astro](https://astro.build/), so feel free to contribute.
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [require("@tailwindcss/typography"), require("daisyui")],
-	daisyui: {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
     themes: ["light", "night"],
-	darkTheme: "night"
-  	},
-
-}
+    darkTheme: "night",
+  },
+};
